9352ef568eccf004a923bf4d72979fd9a4466039
Thelaris <kurt.widdup@gmail.com>
2017-05-21 01:17:08 +1000

    Problem 03 Complete

--- Problem-03/Problem-03/Program.cs
+++ Problem-03/Problem-03/Program.cs
@@ -10,6 +10,24 @@ namespace Problem_03
     {
         static void Main(string[] args)
         {
+            long n = 600851475143;
+            int i = 2;
+            int answer = 0;
+
+            List<long> factors = new List<long>();
+            while (n > 1)
+            {
+                while (n % i == 0)
+                {
+                    factors.Add(i);
+                    n /= i;
+                }
+                i += 1;
+            }
+            answer = Convert.ToInt32(factors.Max());
+
+            Console.WriteLine(answer);
+            Console.ReadLine();
         }
     }
 }
