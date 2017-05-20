75bd4edd44ab2260d1ea4983fcec36b5c182c98d
Thelaris <kurt.widdup@gmail.com>
2017-05-21 02:13:12 +1000

    Problem 04 Complete

--- Problem-04/Problem-04/Program.cs
+++ Problem-04/Problem-04/Program.cs
@@ -8,8 +8,44 @@ namespace Problem_04
 {
     class Program
     {
+
+        public static Boolean IsPalindrome(int n)
+        {
+            int rev = 0;
+            int x = n;
+
+            while (x > 0)
+            {
+                rev = 10 * rev + x % 10;
+                x /= 10;
+            }
+
+            return n == rev;
+        }
+
         static void Main(string[] args)
         {
+            int max = 100001;
+
+            for (int i = 999; i >= 100; i--)
+            {
+                if (max >= i * 999)
+                {
+                    break;
+                }
+                for (int j = 999; j >= 100; j--)
+                {
+                    int k = i * j;
+                    if (max < k && IsPalindrome(k))
+                    {
+                        max = k;
+                        Console.WriteLine(i);
+                        Console.WriteLine(j);
+                    }
+                }
+            }
+            Console.WriteLine(max);
+            Console.ReadLine();
         }
     }
 }
