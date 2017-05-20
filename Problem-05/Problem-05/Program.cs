893d8815bc2e068684b8a0b0b7c006ea210e6ff2
Thelaris <kurt.widdup@gmail.com>
2017-05-21 02:44:21 +1000

    Problem 05 Completed

--- Problem-05/Problem-05/Program.cs
+++ Problem-05/Problem-05/Program.cs
@@ -10,6 +10,31 @@ namespace Problem_05
     {
         static void Main(string[] args)
         {
+            int n = 1;
+            bool zeroRemainder = false;
+
+            while (n > 0)
+            {
+                for (int i = 1; i <= 20; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        zeroRemainder = true;
+                    }
+                    else
+                    {
+                        zeroRemainder = false;
+                        n++;
+                        break;
+                    }
+                }
+                if (zeroRemainder)
+                {
+                    Console.WriteLine(n);
+                    Console.ReadLine();
+                    break;
+                }
+            }
         }
     }
 }
