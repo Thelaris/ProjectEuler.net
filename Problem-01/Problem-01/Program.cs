f103c759d5cc75b46ee01c35cc7a94fd2649de84
Thelaris <kurt.widdup@gmail.com>
2017-05-20 23:48:56 +1000

    Completed Problem 01

--- Problem-01/Problem-01/Program.cs
+++ Problem-01/Problem-01/Program.cs
@@ -10,6 +10,18 @@ namespace Problem_01
     {
         static void Main(string[] args)
         {
+            int total = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    total += i;
+                }
+            }
+
+            Console.WriteLine(total);
+            Console.ReadLine();
         }
     }
 }
