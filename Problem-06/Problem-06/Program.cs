d5b4b4e238b7e245a92385a973397c68864ffbe1
Thelaris <kurt.widdup@gmail.com>
2017-05-21 02:57:19 +1000

    Small structuring fix

--- Problem-06/Problem-06/Program.cs
+++ Problem-06/Problem-06/Program.cs
@@ -13,6 +13,7 @@ namespace Problem_06
             int sumOfSquares = 0;
             int squareOfSum = 0;
             int answer = 0;
+
             for (int i = 1; i <= 100; i++)
             {
                 sumOfSquares += i * i;
