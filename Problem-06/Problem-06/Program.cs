dec67feb0393ea2f4f0a26466173efcc479d47dd
Thelaris <kurt.widdup@gmail.com>
2017-05-21 02:56:21 +1000

    Problem 06 Completed

--- Problem-06/Problem-06/Program.cs
+++ Problem-06/Problem-06/Program.cs
@@ -10,6 +10,25 @@ namespace Problem_06
     {
         static void Main(string[] args)
         {
+            int sumOfSquares = 0;
+            int squareOfSum = 0;
+            int answer = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                sumOfSquares += i * i;
+            }
+
+            for (int j = 1; j <= 100; j++)
+            {
+                squareOfSum += j;
+            }
+
+            squareOfSum *= squareOfSum;
+
+            answer = squareOfSum - sumOfSquares;
+
+            Console.WriteLine(answer);
+            Console.ReadLine();
         }
     }
 }
